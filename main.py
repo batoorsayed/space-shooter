2844af893ef879418ba034ed954e349685f4c71f
Batoor <123397108+batoorsayed@users.noreply.github.com>
2025-07-28 14:30:50 -0700

    Add initial game assets and setup main.py for pygame initialization. Add .venv config to pyproject.toml

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from space-shooter!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ src/main.py
@@ -0,0 +1,5 @@
+import pygame
+
+# Init
+pygame.init()
+display_surface = pygame.display.set_mode
